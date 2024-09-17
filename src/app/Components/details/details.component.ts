a282aa46693c305ec7c6d6ae6366cf8cb079cbc8
Alejandro <nestorale.salguero@outlook.es>
2024-09-17 00:40:25 -0300

    corrigiendo repetidos2

--- src/app/Components/details/details.component.ts
+++ src/app/Components/details/details.component.ts
@@ -19,6 +19,6 @@ export class DetailsComponent {
     const usuariosBuscados = this.producto.filter(producto => producto.name === name);
 
     this.item = usuariosBuscados
-    console.log(this.item)
+
   }
 }
